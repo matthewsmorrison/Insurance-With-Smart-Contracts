1cb46c4008e2044687bd3f97cd3b740e95b487a5
Matt <matthew.seamus.morrison@gmail.com>
2018-04-04 16:16:21 +0100

    Fixed Tests

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 var Insurance = artifacts.require("./Insurance.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Insurance);
-=======
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
->>>>>>> fe9eaebd361049aec59a4635198f643867162e26
 };
